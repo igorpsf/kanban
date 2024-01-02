8493e19211d962945d4ab7f88ca8e7432d62943f
Igor Postnikov <igor.postnikov8@gmail.com>
2024-01-02 12:09:42 -0800

    Updated statuses link

--- src/api/StatusesServices.js
+++ src/api/StatusesServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function fetchStatuses() {
-    const response = await axios.get('http://3.101.20.102:5000/api/statuses')
+    const response = await axios.get('https://kanban-server-qq74.onrender.com/statuses')
     return response
 }
